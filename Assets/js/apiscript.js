c773cc238ed1a71460a1c57eec9225ebf17dd11c
Giovanni <gdomino01@gmail.com>
2021-12-03 19:39:39 -0700

    Added the favorites system

--- Assets/js/apiscript.js
+++ Assets/js/apiscript.js
@@ -2,8 +2,42 @@ const searchInput = document.getElementById("search-here");
 const searchButton = document.getElementById("search-btn");
 const articlesDiv = document.getElementById("ex1-tabs-1");
 const videosDiv = document.getElementById("ex1-tabs-2");
+const favDiv = document.getElementById("favorites-container");
 
 const apiKey = "AIzaSyDW_VEGzWHTEaftCppwRMklcHH3tpPUBdU";
+
+let favorites = [];
+
+function getFavorites(){
+   favorites = JSON.parse(localStorage.getItem("savedFavorites") || "[]");
+  
+}
+
+function  setFavorite(clickedId){
+  // "do this for the title" const favTitle = ....
+  const favUrl = document.getElementById(clickedId+"-url").value;
+  const newFav = {
+    url: favUrl,
+  };
+  getFavorites();
+  if (favorites.length === 0 || favorites.some((fav)=>{return fav.url === newFav.url})) {
+    return listFavorites();
+  }
+  
+  favorites.push(newFav);
+  localStorage.setItem("savedFavorites", JSON.stringify(favorites));
+  listFavorites();
+}
+
+function listFavorites(){
+  getFavorites();
+  let html = "<ul>";
+  favorites.forEach((fav) => {
+    html += `<li><a href="${fav.url}">${fav.url}</a></li>`;
+  });
+  html += "</ul>";
+  favDiv.innerHTML = html;
+  }
 
 //#region Youtube
 function showYoutubeResults(response) {
@@ -16,6 +50,8 @@ function showYoutubeResults(response) {
     let videoId = item.id.videoId;
     let url = `https://www.youtube.com/watch?v=${videoId}`;
     let thumbnail = item.snippet.thumbnails.default.url;
+    let favData = `<input id = "fav-btn-${item.id.videoId}-url" value = "${url}" type = "hidden"></input>`
+    let favBtn = `<button id = "fav-btn-${item.id.videoId}">Favorite</button>`;
     html +=
       '<div class="result-item-base youtube-result-item">' +
       `<a href="${url}">` +
@@ -23,7 +59,7 @@ function showYoutubeResults(response) {
       '<div class="result-item-text">' +
       `<h1>V: ${title}</h1>` +
       `<p>${description}</p>` +
-      "</div></a></div>";
+      "</div></a>"+favData+favBtn+"</div>";
   });
 
   videosDiv.innerHTML = html;
@@ -56,7 +92,7 @@ function showGoogleResults(response) {
   let html = "";
   let searchResults = response.items;
 
-  searchResults.forEach((item) => {
+  searchResults.forEach((item,index) => {
     let title = item.title;
     let url = item.link;
     let thumbnail;
@@ -64,13 +100,15 @@ function showGoogleResults(response) {
     if (pagemap && pagemap.cse_thumbnail) {
       thumbnail = pagemap.cse_thumbnail[0].src;
     }
+    let favData = `<input id = "fav-btn-${index}-url" value = "${url}" type = "hidden"></input>`
+    let favBtn = `<button id = "fav-btn-${index}" onClick = "setFavorite(this.id)">Favorite</button>`;
     html +=
       '<div class="result-item-base google-result-item">' +
       `<a href="${url}">` +
       (thumbnail ? `<img src="${thumbnail}" width="128" height="128">` : "") +
       '<div class="result-item-text">' +
       `<h1>A: ${title}</h1>` +
-      "</div></a></div>";
+      "</div></a>"+favData+favBtn+"</div>";
   });
 
   articlesDiv.innerHTML = html;
@@ -104,3 +142,5 @@ searchButton.addEventListener("click", function (e) {
   searchYoutube();
   searchGoogle();
 });
+
+listFavorites();
